5a00f09eb63255e63217b6ac47616076c7197326
Lerros <108396158+LerrosGr@users.noreply.github.com>
2024-03-12 20:00:25 +0200

    update

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -5,28 +5,30 @@ const input = form.querySelector('input');
 const localStorageKey = 'feedback-form-state';
 
 form.addEventListener('input', evt => {
-  const text = textarea.value.trim();
-  const email = input.value.trim();
-  const data = JSON.stringify({ text, email });
-  localStorage.setItem(localStorageKey, data);
+  const text = evt.currentTarget.elements.message.value.trim();
+  const email = evt.currentTarget.elements.email.value.trim();
+  const objData = JSON.stringify({ text, email });
+  localStorage.setItem(localStorageKey, objData);
 });
-
-const jsn = localStorage.getItem(localStorageKey);
-const data = JSON.parse(jsn);
-if (data !== null) {
-  textarea.value = data.text;
-  input.value = data.email;
-}
 
 form.addEventListener('submit', evt => {
   evt.preventDefault();
-  const text = textarea.value.trim();
-  const email = input.value.trim();
-  if ((text.length > 0) & (email.length > 0)) {
+  if ((textarea.value.length !== 0) & (input.value.length !== 0)) {
+    const jsn = localStorage.getItem(localStorageKey);
+    const data = JSON.parse(jsn);
     console.log(data);
     localStorage.removeItem(localStorageKey);
     form.reset();
   } else {
-    console.log('please fill all field');
+    alert('Please, fill all fields!');
   }
 });
+
+const jsn = localStorage.getItem(localStorageKey) ?? '';
+try {
+  const data = JSON.parse(jsn);
+  textarea.value = data.text;
+  input.value = data.email;
+} catch {
+  console.log('No saved data');
+}
